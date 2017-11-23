26126a94a55acf369e48fa5546d146ce6a3fcf79
Ricardo <richard020797@gmail.com>
2017-11-23 00:57:35 -0600

    Adding celery

---
+++ django_celery/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/hunter/Documents/devf_b17_backend/celery_new/django_celery/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
