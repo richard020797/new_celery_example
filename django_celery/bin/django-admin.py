5ba9b2fafa6b6e63a6d8f0fb88d7b4b37b88eba2
Ricardo <richard020797@gmail.com>
2017-11-23 17:49:30 -0600

    New setup

--- django_celery/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/hunter/Documents/devf_b17_backend/celery_new/django_celery/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
---
+++ env_celery/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/richi/Documents/backend-b17/new_celery/env_celery/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
